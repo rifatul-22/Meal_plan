b3983c558fcbca93deebf170fae068196ce1aea2
rifatul-22 <rifatulislam455@gmail.com>
2022-11-06 19:46:09 +0600

    2

--- js/app.js
+++ js/app.js
@@ -42,19 +42,3 @@ $(function(){
         })
     });
 })
-
-
-
-
-// var darkMode_icon = document.getElementById('mode');
-// darkMode_icon.addEventListener('click', function(){
-//     document.body.classList.toggle('darkMode');
-
-//     if(darkMode_icon.classList.contains('fa-moon')){
-//         darkMode_icon.classList.add('fa-sun');
-//         darkMode_icon.classList.remove('fa-moon');
-//     }else{
-//         darkMode_icon.classList.remove('fa-sun');
-//         darkMode_icon.classList.add('fa-moon');
-//     }
-// });
